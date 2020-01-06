9005cdf150a6f64bfd42cb055186ced44851aacb
Kelley Shann <kelleyshann@gmail.com>
2020-01-05 19:30:33 -0500

    task: updated connection module

--- Database/connection.py
+++ Database/connection.py
@@ -1,37 +1,7 @@
-from Database.config import config
 import psycopg2
 
 
 def connect():
-    """Connecting to the PostgreSQL server"""
-    conn = None
-    try:
-        params = config()
+    conn = psycopg2.connect(user='postgres', password='password', database='yourdatabase',
+                            host='localhost', port='5432')
 
-        # connect to the postgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version: ')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # closing the communication with the PostreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
