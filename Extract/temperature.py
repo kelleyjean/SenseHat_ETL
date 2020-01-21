5b82cf0ac71565874779547e51df2e9dc72ecba6
kelleyjean <kelleyshann@gmail.com>
2020-01-21 17:07:40 -0500

    task: updated temperature function

--- Extract/temperature.py
+++ Extract/temperature.py
@@ -5,11 +5,12 @@ sense = SenseHat()
 
 
 def get_temperature():
-
+	temp_list = []
 	temp_c = sense.get_temperature()
 	temp_f = ((temp_c/5) * 9) + 32
-
 	timestamp = datetime.utcnow().isoformat()
-
-	return temp_c, temp_f
+	temp_list.append(temp_c)
+	temp_list.append(temp_f)
+	temp_list.append(timestamp)
+	return temp_list
 
